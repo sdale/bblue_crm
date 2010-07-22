45f1d8a8456716d141767dcab2093a7a49080117
Pedro Mateus Tavares <pedromtavares@pedromtavares-desktop.(none)>
2010-07-22 04:04:14 -0300

    Final version before open sourcing[2]

--- app/batch_book/caching_support.rb
+++ app/batch_book/caching_support.rb
@@ -18,7 +18,7 @@ module CachingSupport
       when 'eager'
         last_id = Rails.cache.read('last_id')
         last_time = Rails.cache.read('last_time') || Time.now
-        last_object = self.find_without_caching(:last, :params => {:updated_since => last_time})
+        last_object = self.find(:last, :params => {:updated_since => last_time}, :raw => true )
         self.recache if last_id.nil? || (last_object && last_id != last_object.id)
     end
     YAML::load(Rails.cache.read(self.name))
